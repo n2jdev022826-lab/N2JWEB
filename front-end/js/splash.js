7e2172d1ef49d95fa573ec8f86a59362856c2ebf
Jaztin-F <fjustine920@gmail.com>
2026-03-01 21:44:57 +0800

    initial development

---
+++ front-end/js/splash.js
@@ -0,0 +1,39 @@
+let intro = document.querySelector(".intro");
+let logoSpan = document.querySelectorAll(".logo");
+let logoImg = document.querySelector(".logo-image");
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Animate image first
+  setTimeout(() => {
+    logoImg.classList.add("active");
+  }, 400);
+
+  // Animate text letters
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+  }, 800);
+
+  // Fade text letters
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.remove("active");
+        span.classList.add("fade");
+      }, (idx + 1) * 80);
+    });
+  }, 2800);
+
+  // Fade out splash
+  setTimeout(() => {
+    intro.classList.add("hide");
+  }, 3200);
+
+  // Redirect
+  setTimeout(() => {
+    window.location.href = "/N2JWEB/landing-page";
+  }, 4000);
+});
